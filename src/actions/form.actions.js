8527c4a022aa8f14a331e2de0b1d42cff7579a50
wanderer911 <dp160291sas@gmail.com>
2018-08-28 12:29:31 +0300

    delete unnecessary redux code

--- src/actions/form.actions.js
+++ src/actions/form.actions.js
@@ -2,14 +2,8 @@ import {formConstants} from '../constants'
 
 export const formActions = {
     changeInputValue,
-    //getInputValues
 }
 
-// function getInputValues(){
-//     return dispatch=>{
-//         dispatch({type:formConstants.GET_INPUT_VALUES});
-//     }
-// }
 
 function changeInputValue(name,value){
     return dispatch=>{
