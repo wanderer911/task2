73e97f7e99484045e205861aaa02aa3914163bbc
wanderer911 <dp160291sas@gmail.com>
2018-11-06 17:45:03 +0200

    checkbox test fixed

--- test/drivers/elementsDriver/checkboxDriver.js
+++ test/drivers/elementsDriver/checkboxDriver.js
@@ -4,9 +4,6 @@ module.exports = el => ({
   toggle: () => {
     const checkbox = el.querySelector('input[type=checkbox]')
     checkbox.checked = !checkbox.checked;
-    // checkbox.value = value?'on':'off';
-    // console.log(checkbox.outerHTML,checkbox.value)
-    // checkbox.value = 'off'
     triggerEvent(checkbox, 'click')
   }
 })
